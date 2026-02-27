a9a5e019c89cc09df04911c2eaad9c2fcdcb8067
Ayib201 <ayibt2612@gmail.com>
2026-02-27 17:21:34 +0000

    first commit

---
+++ src/main/java/com/groupeisi/commerceenligne/CommerceEnLigneApplication.java
@@ -0,0 +1,13 @@
+package com.groupeisi.commerceenligne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceEnLigneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceEnLigneApplication.class, args);
+	}
+
+}
