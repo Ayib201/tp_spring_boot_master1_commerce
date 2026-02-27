cd408f568c70534fb2f1c8a5a8f329846aad62bc
Ayib201 <ayibt2612@gmail.com>
2026-02-27 18:02:13 +0000

    Connexion a la base de donnee, ajout du pipeline et des dependances

--- src/main/java/com/groupeisi/commerceenligne/CommerceEnLigneApplication.java
+++ src/main/java/com/groupeisi/commerceenligne/CommerceEnLigneApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CommerceEnLigneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommerceEnLigneApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceEnLigneApplication.class, args);
+    }
 
 }
